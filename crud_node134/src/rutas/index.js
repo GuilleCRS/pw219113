a2084daa406e6971f73f97267670457a2396e8ff
GuilleCRS <guillermocrs@gmail.com>
2019-04-29 14:00:28 -0600

    Conexion a la base de datos

--- crud_node134/src/rutas/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const rutas = express.Router();
-
-//GET es lo que solicita el navegador del cliente
-rutas.get("/",(req,res) => {
-	res.send("Hola mundo Node");
-});
-module.exports = rutas; 
