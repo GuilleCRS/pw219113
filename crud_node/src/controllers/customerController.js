49e3fc0b2974bd7c984802ee44eabf8597ea462b
GuilleCRS <guillermocrs@gmail.com>
2019-04-30 13:58:31 -0600

    Listado de clientes y controlador

---
+++ crud_node/src/controllers/customerController.js
@@ -0,0 +1,20 @@
+const controller = {
+
+};
+
+controller.list = (req,res) => {
+	req.getConnection((err,conn)=> {
+		conn.query('SELECT * FROM customers',(err,customers)=>{
+			if(err){
+				res.json(err);
+			}
+			console.log(customers);
+
+		});
+
+	});
+
+
+};
+
+module.exports = controller;
--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,16 @@
 const express=  require('express');
 const rutas= express.Router();
+const customerController=require('../controllers/customerController');
 
+/*
 rutas.get("/", (req,res)=>{
 	res.send("Hola mundo en node ");
 });
+*/
+rutas.get('/',customerController.list);
+//rutas.post('/add',customerController.save);
+//rutas.get('/delete',customerController.delete);
+//rutas.get('/update/:id',customerController.edit);
+//rutas.post('/update/:id',customerController.update)
 
 module.exports = rutas;
